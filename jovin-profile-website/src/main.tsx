6ad5b74fa84e0d924b0ee5b0251198d9f468b8f3
Jovin Najwan <jovin.najwan@gmail.com>
2026-09-15 20:43:06 +0700

    installasi

--- jovin-profile-website/src/main.tsx
+++ jovin-profile-website/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { LandingPage } from "./landingPage";
+import { LandingPage } from "./landingpage";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 const router = createBrowserRouter([
